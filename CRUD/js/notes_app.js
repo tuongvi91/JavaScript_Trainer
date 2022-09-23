bfc7fd84968adf20a318902a52bfd275d7b7ac9f
tuongvi91 <tuongvits77@gmail.com>
2022-09-23 22:54:20 +0700

    CRUD notes app

---
+++ CRUD/js/notes_app.js
@@ -0,0 +1,42 @@
+var list = [];
+if (JSON.parse(localStorage.getItem("todos"))) {
+    list = JSON.parse(localStorage.getItem("todos"));
+} else {
+    localStorage.setItem("todos", JSON.stringify(list));
+}
+function showWork() {
+    let row = "";
+    for (var i in list) {
+        row +=`<div id="listWorkSpace"`+"<tr >"
+            + `<div id="content">${list[i]}</div>`
+            +`<button id="edit" onclick='onEdit(${i})'>Edit</button>` 
+            +`<button id="delete" onclick='onDelete(${i})'>Delete</button>`
+            +"</tr>"+`</div><hr></hr>`;
+    }
+    document.getElementById("listWork").innerHTML = row;
+};
+showWork();
+function onSubmit(i) {
+    var todos = document.getElementById("enterTask").value;
+    list.push(todos);
+    localStorage.setItem("todos", JSON.stringify(list));
+    window.location.reload();
+    //localStorage.clear();
+}
+
+function onDelete(i) {
+    list.splice(i, 1);
+    localStorage.setItem("todos", JSON.stringify(list));
+    window.location.reload();
+}
+function onEdit(i) {
+    var showEdit = "";
+    showEdit += `<input id="editTodos" value = "${list[i]}"></input>`;
+    showEdit += `<button id = "submitEditButton" onclick='submitEdit(${i})'>Submit</button>`;
+    document.getElementById("showEditComponent").innerHTML = showEdit;
+}
+function submitEdit(i) {
+    list[i] = document.getElementById("editTodos").value;
+    localStorage.setItem("todos", JSON.stringify(list));
+    window.location.reload();
+}
